5f318d92d1fae635f790c1b37988e097116e5985
yunusbasaran <coursium@gmail.com>
2020-04-03 05:01:04 +0200

    image file, java file added

--- app/src/test/java/com/gmbh/internetstores/ExampleUnitTest.java
+++ app/src/test/java/com/gmbh/internetstores/ExampleUnitTest.java
@@ -1,17 +1,23 @@
 package com.gmbh.internetstores;
 
+import android.content.Context;
+import androidx.test.platform.app.InstrumentationRegistry;
+import com.gmbh.internetstores.frameworks.data.model.ISBikesData;
+import com.gmbh.internetstores.frameworks.data.pref.PrefData;
+import java.util.List;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RuntimeEnvironment;
 
 import static org.junit.Assert.*;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
+
+
 }
